3c6294ccaae9893c0eb3e296447ccea3e8881bab
Vaibhav Agarwal <vaibhav.geit@gmail.com>
2012-11-15 11:48:32 -0800

    changes to be ignored

--- gen/edu/usc/vakacalendar/R.java
+++ gen/edu/usc/vakacalendar/R.java
@@ -15,11 +15,13 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int CameraButton=0x7f070001;
-        public static final int EventListButton=0x7f070003;
-        public static final int StartButton=0x7f070002;
-        public static final int menu_settings=0x7f070004;
-        public static final int textView1=0x7f070000;
+        public static final int CameraButton=0x7f070003;
+        public static final int EventListButton=0x7f070005;
+        public static final int StartButton=0x7f070004;
+        public static final int listView1=0x7f070001;
+        public static final int menu_settings=0x7f070006;
+        public static final int scrollView1=0x7f070000;
+        public static final int textView1=0x7f070002;
     }
     public static final class layout {
         public static final int activity_camera_screen=0x7f030000;
